8b43b3f0310d7e9b5587233f9ace6ce4b559beea
DVorortnoy <109036061+sklef5@users.noreply.github.com>
2023-03-14 15:38:10 +0200

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,5 @@
 /** add your listiener here */
 export const refs = {
-    cocktailsList: document.querySelector('ul.CocktailsList'),
+    cocktailsList: document.querySelector('ul.cocktails-list'),
    
 };
