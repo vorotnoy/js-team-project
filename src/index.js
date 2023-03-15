7a7749c11834f4557b1af11a963740e56e9be677
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-03-15 15:53:43 +0200

    rename addTo.js for toggleFavoriteCocktail.js
    add color variable for #heart in symbol-defs.svg;
    update index.js;
    update renderCocktails.js;
    update _mainblock.scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { returnCocktails } from "./js/mainblock/mainblock";
 import { refs } from "./js/refs";
-import { addToFavorites } from "./js/mainblock/addTo";
+import { toggleFavorites } from "./js/mainblock/toggleFavoriteCocktail";
 
 
 returnCocktails();
-refs.cocktailsList.addEventListener('click', addToFavorites);
+refs.cocktailsList.addEventListener('click', toggleFavorites);
--- src/js/mainblock/addTo.js
+++
@@ -1,9 +0,0 @@
-export function addToFavorites(evt) {
-    const favoriteCocktails = [];
-    const target = evt.target.classList;
-    if (target.contains('addTo')) {
-        console.log('click');
-
-    }
-    // localStorage.setItem("favoriteCocktails", JSON.stringify(favoriteCocktails));
-};
