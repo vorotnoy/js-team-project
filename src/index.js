8c1ef3865061d0d332f59bb1721df5567b16125d
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-03-15 10:30:10 +0200

    create hero js files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import { getRandomCocktail } from "./js/mainblock/renderCocktails";
+import { onSelectBtnClick } from   "./js/hero/onClickFunctions";
+import { onAlphabetBtnClick } from "./js/hero/onClickFunctions";
+import { refs } from "./js/refs";
+
+const { selectBtn, alphabet } = refs;
+
+selectBtn.addEventListener('click', onSelectBtnClick);
+alphabet.addEventListener('click', onAlphabetBtnClick);
 
 getRandomCocktail();
 getRandomCocktail();
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,11 @@
 /** add your listiener here */
 export const refs = {
     cocktailsList: document.querySelector('ul.cocktails-list'),
-   
+    cocktailsTitle:  document.querySelector('.cocktails-title'),
+    selectMenu: document.querySelector('#select__menu'),
+    selectBtn: document.querySelector('#select__btn'),
+    alphabet: document.querySelector('#alphabet'),
+    noCocktails: document.querySelector('.no-cocktails'),
 };
+
+
