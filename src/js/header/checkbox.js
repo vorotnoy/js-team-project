8f9a9c4c97fab09a4b42cf45fd85afb52a54ab0d
DVorortnoy <dimchik505@gmail.com>
2023-03-16 12:50:31 +0200

    add theme fix

--- src/js/header/checkbox.js
+++ src/js/header/checkbox.js
@@ -1,34 +1,20 @@
 import { refs } from '../refs';
-const { checkboxOn, bodyOn } = refs;
-
-const clrwh = document.querySelectorAll('.js-changeclrwh');
-const clror = document.querySelectorAll('.js-changeclror');
-const clrgr = document.querySelectorAll('.js-changeclrgr');
-const bgclr = document.querySelector('.js-changebgrclr');
+const { checkboxOn, bodyOn, clrwh, clror, clrgr, bgclr} = refs;
 
 /** change theme */
 function checkBtn() {
   if (checkboxOn.checked) {
     bodyOn.classList.toggle('js-bgcblack');
     bgclr.classList.toggle('js-bgcblack');
-    clrwh.forEach(item => {
-      item.classList.toggle('js-colorwhite');
-    });
-    clror.forEach(item => {
-      item.classList.toggle('js-colororange');
-    });
-    clrgr.forEach(item => {
-      item.classList.toggle('js-colorgray');
-    });
+    clrwh.forEach(item => {item.classList.toggle('js-colorwhite')});
+    clror.forEach(item => {item.classList.toggle('js-colororange') });
+    clrgr.forEach(item => {item.classList.toggle('js-colorgray')});
   } else {
     bodyOn.classList.toggle('js-bgcblack');
     bgclr.classList.toggle('js-bgcblack');
-    clror.forEach(item => {
-      item.classList.toggle('js-colororange');
-    });
-    clrgr.forEach(item => {
-      item.classList.toggle('js-colorgray');
-    });
+    clrwh.forEach(item => {item.classList.toggle('js-colorwhite')});
+    clror.forEach(item => {item.classList.toggle('js-colororange') });
+    clrgr.forEach(item => {item.classList.toggle('js-colorgray')});
   }
 }
 
