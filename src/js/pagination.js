3ddfb1d5a7d996e5184d7789d3255aa59fefb4f7
PiLoT-P <59173880+PiLoT-P@users.noreply.github.com>
2023-03-17 01:01:40 +0200

    update

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,4 +1,9 @@
-export const ulTag = document.querySelector('.pagination-list');
+import { refs } from './refs';
+import { getValue} from './header/searchbyname';
+import { renderAddRemoveDrinkButton } from './favourites';
+
+const { prewButton, nextButton, ulTag, cocktailsList } = refs;
+let page = 1;
 
 export function pagination(totalPages, page) {
     let liTag = '';
@@ -31,8 +36,11 @@ export function pagination(totalPages, page) {
             }
         }
     } else {
+        for (let i = 1; i < page; i++) {
+            liTag += `<li class="pagination-item"><button class="pagination-number" type="button">${i}</button></li>`;
+        }
         for (let i = page; i <= totalPages; i++) {
-            if (i = page) {
+            if (i === page) {
                 liTag += `<li class="pagination-item"><button class="pagination-number active" type="button">${i}</button></li>`;
             } else {
                 liTag += `<li class="pagination-item"><button class="pagination-number" type="button">${i}</button></li>`;
@@ -41,4 +49,77 @@ export function pagination(totalPages, page) {
     }
     ulTag.innerHTML = liTag;
     
+}
+
+function reloadButton(totalPage, page) {
+    if (page === 1) {
+        prewButton.setAttribute('disabled', 'true');
+    } else {
+        prewButton.removeAttribute('disabled');
+    }
+    if (page === totalPage) {
+        nextButton.setAttribute('disabled', 'true');
+    } else {
+        nextButton.removeAttribute('disabled');
+    }
+}
+
+async function loadMor(event) {
+    if (event.target.classList.contains('pagination-arrow') || event.target.classList.contains('pagination-number')) {
+        page = Number(event.target.textContent);
+        cocktailsList.innerHTML = createListItem(getValue[page - 1]);
+        reloadButton(getValue.length, page);
+        pagination(getValue.length, page);
+    } else {
+        return;
+    }
+}
+
+async function prewList(event) {
+    page -= 1;
+    cocktailsList.innerHTML = createListItem(getValue[page - 1]);
+    reloadButton(getValue.length, page);
+    pagination(getValue.length, page);
+}
+
+export async function nextList(event) {
+    page += 1;
+    cocktailsList.innerHTML = createListItem(getValue[page - 1]);
+    reloadButton(getValue.length, page);
+    pagination(getValue.length, page);
+}
+
+ulTag.addEventListener('click', loadMor);
+
+prewButton.addEventListener('click', prewList);
+
+nextButton.addEventListener('click', nextList);
+
+function createListItem(data) {
+    let cocktailsMarkup = '';
+    data.map(
+      dat =>
+        (cocktailsMarkup += `
+        <li class="cocktail-item">
+            <div class="cocktail-card">
+                <a class="cocktail-link" href="#">
+                    <img class="cocktail-picture" src="${
+                      dat.strDrinkThumb
+                    }" alt="${dat.strDrink}">
+                </a>
+                    <p class="cocktail-label">${dat.strDrink}</p>
+                     <div class="cocktail-card-btn-wrapper">
+                    <button class="learnMore">Learn more</button>
+                   ${renderAddRemoveDrinkButton(
+                      dat.idDrink,
+                     dat.strDrink,
+                     dat.strDrinkThumb
+                   )}
+            </div>
+            </div>
+        </li>
+    `)
+    );
+
+  return cocktailsMarkup;
 }
