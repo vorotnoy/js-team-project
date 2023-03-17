c47cf9fb5062282d7bfe42ba8f8e5df97a4d9128
DVorortnoy <109036061+sklef5@users.noreply.github.com>
2023-03-17 13:39:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const pathname = window.location.pathname;
 const location = pathname.split('/').pop();
 
 
-if (location === '') {
+if (location === ''||location === 'index.html') {
   returnCocktails();
   //cocktailsList.addEventListener('click', favouritesClickEvent);
 
