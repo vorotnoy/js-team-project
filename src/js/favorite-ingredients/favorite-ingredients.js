9b102350d39c84843cb0652a9d3f03664659991a
IhorC <igorchua@gmail.com>
2023-03-17 19:46:45 +0200

    ingredients "Remove" btn still doesnt work

--- src/js/favorite-ingredients/favorite-ingredients.js
+++ src/js/favorite-ingredients/favorite-ingredients.js
@@ -1,22 +1,17 @@
 import {attachIngredientEvents} from "../modallearnmore/modal-learn-more-ingredient";
 import { getFavouriteIngredients, renderAddRemoveIngredientButton } from '../favourites';
-import {initializeFavourites} from '../favorite-cocktails/favorite';
 
-// const pathname = window.location.pathname;
-const refs = {
-    favIngredientsList: document.querySelector('.fav-ingr__list'),
-    favIngredientsTitle: document.querySelector('.fav-ingr__title') 
-}
 const favIngredientsList = document.querySelector('.fav-ingr__list');
 const favIngredientsTitle = document.querySelector('.fav-ingr__title');
 
 export function initializeFavouritesIng() {
     let windowWidth = window.innerWidth;
-    const localStorageLength = getFavouriteIngredients().length;
+    const localStorageLength = JSON.parse(localStorage.getItem('favorite-cocktail'));
     
-    if (localStorageLength === 0) {
-        favIngredientsTitle.textContent = 'You didn\'t choose any ingredients.'
-        return;
+    // console.log(JSON.parse(localStorage.getItem('favorite-ingredient')));
+    if (localStorageLength === null || localStorageLength.length === 0) {
+        favIngredientsTitle.textContent = 'You didn\'t choose any cocktail.'
+      return;
     }
     if (windowWidth < 768) {
         favIngredientsMarkup(0, 3);
@@ -27,16 +22,24 @@ export function initializeFavouritesIng() {
     }
   }
 
+function drinkCheck(ingr){
+    if(ingr === 'null'){
+        return 'Not a drink';
+    } else {
+        return ingr;
+    }
+}
+
 function favIngredientsMarkup(start, end){
     const cocktailsArr = getFavouriteIngredients();
     let arr = cocktailsArr.slice(start, end);
     
     favIngredientsList.innerHTML = arr.map(e => 
         `<li class="fav-ingr__item">
-            <img src="https://www.thecocktaildb.com/images/ingredients/${e.name}-Small.png" height="100" width="100" alt="${e.name}" />
             <h3 class="fav-ingr__item-title">${e.name}</h3>
+            <p class="fav-ingr__type">${drinkCheck(e.type)}</p>
             <div class="fav-ingr__buttons">
-                <button class="ingredient-link" data-name="${e.name}" data-type="${e.type}" data-modal-open-2>Learn more</button>
+                <button class="ingredient-link learnMore" data-name="${e.name}" data-type="${e.type}" data-modal-open-2>Learn more</button>
                 ${renderAddRemoveIngredientButton(e.name, e.type)}
             </div>
         </li>`
