b557c9c69d1376b7587c4e1ecee585f07eaacab3
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-03-21 11:20:48 +0200

    hero js code refactoring

--- src/js/hero/createcocktailsmarkupbyviewportsize.js
+++ src/js/hero/createcocktailsmarkupbyviewportsize.js
@@ -1,66 +1,14 @@
-import { renderAddRemoveDrinkButton } from '../favourites';
+import { cocktailsMarkup } from '../cocktailsmarkup';
+import { cocktailsMarkupByCounter } from '../cocktailsmarkupbycounter';
 
-export function createCocktailsMarkupByViewportSize(setSize, { data }) {
-  let cocktailsMarkup = '';
+export function createCocktailsMarkupByViewportSize(setSize, data) {
 
-  if (data.drinks.length < setSize) {
-    data.drinks.map(
-      data =>
-        (cocktailsMarkup += `
-        <li class="cocktail-item">
-            <div class="cocktail-card">
-                <a class="cocktail-link" href="#" data-modal-open>
-                    <img class="cocktail-picture" src="${
-                      data.strDrinkThumb
-                    }" alt="${data.strDrink}"  data-id="${data.idDrink}">
-                </a>
-                    <p class="cocktail-label">${data.strDrink}</p>
-                     <div class="cocktail-card-btn-wrapper">
-                    <button class="learnMore" data-id="${
-                      data.idDrink
-                    }" data-modal-open >Learn more</button>
-                   ${renderAddRemoveDrinkButton(
-                     data.idDrink,
-                     data.strDrink,
-                     data.strDrinkThumb
-                   )}
-            </div>
-            </div>
-        </li>
-    `)
-    );
-
-    return cocktailsMarkup;
+  if (data.length < setSize) {
+    return cocktailsMarkup(data);
   }
 
-  for (let i = 0; i < setSize; i++) {
-    cocktailsMarkup += `
-        <li class="cocktail-item">
-            <div class="cocktail-card">
-                <a class="cocktail-link" href="#" data-modal-open>
-                    <img class="cocktail-picture" src="${
-                      data.drinks[i].strDrinkThumb
-                    }" alt="${data.drinks[i].strDrink}" data-id="${
-      data.drinks[i].idDrink
-    }">
-                </a>
-                    <p class="cocktail-label">${data.drinks[i].strDrink}</p>
-
-                    <div class="cocktail-card-btn-wrapper">
-                    <button class="learnMore" data-id="${
-                      data.drinks[i].idDrink
-                    }" data-modal-open>Learn more</button>
-                    ${renderAddRemoveDrinkButton(
-                      data.drinks[i].idDrink,
-                      data.drinks[i].strDrink,
-                      data.drinks[i].strDrinkThumb
-                    )}
-            </div>
-
-            </div>
-        </li>
-    `;
-  }
-
-  return cocktailsMarkup;
+  return cocktailsMarkupByCounter(setSize, data);
 }
+
+
+
