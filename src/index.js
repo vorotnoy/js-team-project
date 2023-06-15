51a0dd63dd887df2fe037303b7adcb5af8d9a97b
DVorortnoy <dimchik505@gmail.com>
2023-06-15 18:00:27 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,40 +1,22 @@
 import { refs } from './js/global/refs';
-import { onClickDropdownOn } from './js/header/dropdown';
-import { checkBtn } from './js/header/checkbox';
 import {defineClick} from './js/cocktailspage/getaction'
 import {location} from './js/global/location'
-
 import {
   returnCocktails,
-  viewportWidthCheck,
-  accumulateCocktails,
-  pourCocktails,
 } from './js/mainblock/mainblock';
-import { favouritesClickEvent } from './js/modalcocktails/changebutton'
-import { getRandomCocktail } from './js/mainblock/rendercocktails';
-
 import { toggleMenu } from './js/header/mobile-menu';
-
 import {
   onSelectBtnClick,
   onAlphabetBtnClick,
 } from './js/hero/onClickFunctions';
-// import { searchCocktail } from './js/header/searchbyname';
-
-import { getCocktailId, updateSize } from './js/favorite-cocktails/favorite';
-
-import { getInputData } from './js/search/searchbyname'
-
 import { initializeFavourites } from './js/favorite-cocktails/favorite';
 import { initializeFavouritesIng } from './js//favorite-ingredients/favorite-ingredients';
 
 const debounce = require('lodash.debounce');
-const { cocktailsList, selectBtn, alphabet, openMenuBtn, closeMenuBtn, toTopButton, openModalBtn } =
+const { selectBtn, alphabet, openMenuBtn, closeMenuBtn, toTopButton } =
 refs;
 
 document.body.addEventListener('click', defineClick);
-
-import {closeModalOnBody} from './js/modalcocktails/closemodal'
 
 openMenuBtn.addEventListener('click', toggleMenu);
 closeMenuBtn.addEventListener('click', toggleMenu);
@@ -43,7 +25,6 @@ closeMenuBtn.addEventListener('click', toggleMenu);
 if (location === ''|| location === 'index.html') {
  
   returnCocktails();
-  //cocktailsList.addEventListener('click', favouritesClickEvent);
 
   selectBtn.addEventListener('click', onSelectBtnClick);
   alphabet.addEventListener('click', onAlphabetBtnClick);
