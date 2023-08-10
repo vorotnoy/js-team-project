a2b896460d2cf92c00a5e3d98008ff1756adbf2a
DVorortnoy <dimchik505@gmail.com>
2023-08-10 18:52:50 +0300

    fix

--- src/js/cocktailspage/getaddremovebutton.js
+++ src/js/cocktailspage/getaddremovebutton.js
@@ -1,41 +1,51 @@
 import { getDrink } from '../localstorage/localstorageforcocktail';
 import * as icons from '../../images/svg/symbol-defs.svg';
 
-export function defuneButton(id, name, image) {
-  if (getDrink(id)) {
-    const title = "Remove"
-    const typeClass = "removeFrom"
-    const svg = "selected"
-    return renderAddRemoveDrinkButton(
-      id,
-      name,
-      image,
-      title = title,
-      typeClass = typeClass,
-      svg = svg
-    );
-  }
-  const title = "Add to"
-  const typeClass = "addTo"
-  const svg = ""
-  return renderAddRemoveDrinkButton(
-    id,
-    name,
-    image,
-    title = title,
-    typeClass = typeClass,
-    svg = svg
-  );
-}
+// export function defuneButton(id, name, image) {
+//   if (getDrink(id)) {
+//     return renderAddRemoveDrinkButton(
+//       id,
+//       name,
+//       image,
+//       (title = 'Remove'),
+//       (typeClass = 'removeFrom'),
+//       (svg = 'selected')
+//     );
+//   }
+//   return renderAddRemoveDrinkButton(
+//     id,
+//     name,
+//     image,
+//     (title = 'Add to'),
+//     (typeClass = 'addTo'),
+//     (svg = '')
+//   );
+// }
 
-export function renderAddRemoveDrinkButton(
-  id,
-  name,
-  image,
-  title,
-  typeClass,
-  svg
-) {
+// export function renderAddRemoveDrinkButton(
+//   id,
+//   name,
+//   image,
+//   title,
+//   typeClass,
+//   svg
+// ) {
+//   return `<button class="favourite ${typeClass}" data-id="${id}" data-name="${name}" data-image="${image}">${title}
+//          <svg class="icon-heart${svg}">
+//              <use href="${icons}#heart"></use>
+//          </svg>
+//        </button>`;
+// }
+
+export function defuneButton(id, name, image) {
+  let title = 'Add to'
+  let typeClass = 'addTo'
+  let svg = ''
+ if (getDrink(id)){
+  title = 'Remove',
+  typeClass = 'removeFrom',
+  svg = 'selected'
+ }
   return `<button class="favourite ${typeClass}" data-id="${id}" data-name="${name}" data-image="${image}">${title}
          <svg class="icon-heart${svg}">
              <use href="${icons}#heart"></use>
